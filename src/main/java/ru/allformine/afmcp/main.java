7202f386cf84fb757f2704c96cbe09641d0c8a88
Iterator <rob.bog@list.ru>
2018-11-22 00:51:57 +0300

    ля, не робит :(

--- src/main/java/ru/allformine/afmcp/main.java
+++ src/main/java/ru/allformine/afmcp/main.java
@@ -8,15 +8,12 @@ import com.comphenix.protocol.events.PacketAdapter;
 import com.comphenix.protocol.events.PacketEvent;
 import com.comphenix.protocol.wrappers.WrappedGameProfile;
 import com.comphenix.protocol.wrappers.WrappedServerPing;
-import org.bukkit.ChatColor;
-import org.bukkit.entity.Entity;
-import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.server.ServerListPingEvent;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.Arrays;
+import java.util.UUID;
 
 public class main extends JavaPlugin implements Listener {
     public void onEnable() {
@@ -25,17 +22,14 @@ public class main extends JavaPlugin implements Listener {
         manager.registerEvents(this,this);
 
         ProtocolLibrary.getProtocolManager().addPacketListener(
-            new PacketAdapter(this, ListenerPriority.NORMAL,
-                Arrays.asList(PacketType.Status.Server.OUT_SERVER_INFO), ListenerOptions.ASYNC) {
+            new PacketAdapter(this, ListenerPriority.NORMAL, Arrays.asList(PacketType.Status.Server.OUT_SERVER_INFO), ListenerOptions.ASYNC) {
+                @Override
+                public void onPacketSending(PacketEvent event) {
+                    WrappedServerPing ping = event.getPacket().getServerPings().read(0);
 
-                    @Override
-                    public void onPacketSending(PacketEvent event) {
-                        WrappedServerPing ping = event.getPacket().getServerPings().read(0);
-
-                        //блять, да пусть жалуется, что deprecated, мне-то что???
-                        ping.setPlayers(Arrays.asList(new WrappedGameProfile("00000000-0000-0000-0000-000000000000",
-                                "AllForMine")));
-                    }
-                });
+                    //сука, таки deprecated было здесь не зря :/
+                    ping.setPlayers(Arrays.asList(new WrappedGameProfile(UUID.randomUUID(), "Lenin_")));
+                }
+            });
     }
 }
