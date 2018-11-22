0cb6dacd69161ad547ae5a677716dc0de8d354b4
Iterator <rob.bog@list.ru>
2018-11-23 02:29:46 +0300

    выпилил изменение списка игроков, это уже есть в VNP

--- src/main/java/ru/allformine/afmcp/main.java
+++ src/main/java/ru/allformine/afmcp/main.java
@@ -6,8 +6,8 @@ import com.comphenix.protocol.events.ListenerOptions;
 import com.comphenix.protocol.events.ListenerPriority;
 import com.comphenix.protocol.events.PacketAdapter;
 import com.comphenix.protocol.events.PacketEvent;
-import com.comphenix.protocol.wrappers.WrappedGameProfile;
 import com.comphenix.protocol.wrappers.WrappedServerPing;
+import org.bukkit.ChatColor;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -27,9 +27,7 @@ public class main extends JavaPlugin implements Listener {
                 public void onPacketSending(PacketEvent event) {
                     WrappedServerPing ping = event.getPacket().getServerPings().read(0);
 
-                    ping.setPlayers(Arrays.asList(new WrappedGameProfile(UUID.randomUUID(), "Lenin_")));
-
-                    System.out.println("AFMCP Test");
+                    ping.setMotD(ChatColor.YELLOW+""+ChatColor.BOLD+"AllForMine SpaceUnion (WIP)");
                 }
             });
     }
