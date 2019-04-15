4c8802853790b9462c9dc00cc952485d315db70b
Rob <rob.bog@list.ru>
2019-04-16 01:30:06 +0300

    Блять, неужели оно заработает???

--- src/main/java/ru/allformine/afmcp/AFMCorePlugin.java
+++ src/main/java/ru/allformine/afmcp/AFMCorePlugin.java
@@ -14,11 +14,9 @@ import org.spongepowered.api.event.game.state.GameStoppingServerEvent;
 import org.spongepowered.api.plugin.Plugin;
 import ru.allformine.afmcp.handlers.EventListener;
 import ru.allformine.afmcp.net.discord.Discord;
-
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 
 @Plugin(
         id = "afmcp",
@@ -36,30 +34,23 @@ public class AFMCorePlugin {
     @Inject
     @ConfigDir(sharedRoot = false)
     private Path configDir;
+    private Path configFile;
 
-    private Path configFile = Paths.get(configDir + "/config.conf");
-
-    private ConfigurationLoader<CommentedConfigurationNode> configLoader = HoconConfigurationLoader.builder().setPath(configFile).build();
+    private ConfigurationLoader<CommentedConfigurationNode> configLoader;
     private static CommentedConfigurationNode configNode;
 
     @Listener
     public void preInit(GamePreInitializationEvent event) {
         Sponge.getEventManager().registerListeners(this, new EventListener());
 
-        if (!Files.exists(configDir)) {
-            try {
-                Files.createDirectories(configDir);
-            } catch (IOException io) {
-                io.printStackTrace();
-            }
-        }
-        setup();
+        configFile = configDir.resolve("config.conf");
+        configLoader = HoconConfigurationLoader.builder().setPath(configFile).build();
+
+        configSetup();
     }
 
     @Listener
     public void onServerStart(GameStartedServerEvent event) {
-        logger.info(configNode.getString("discord.webhooks.url_lvl1"));
-
         Discord.sendMessageServer(Discord.MessageTypeServer.TYPE_SERVER_STARTED);
     }
 
@@ -68,12 +59,19 @@ public class AFMCorePlugin {
         Discord.sendMessageServer(Discord.MessageTypeServer.TYPE_SERVER_STOPPED);
     }
 
-    // Всякая хуйня для конфигов
-    private void setup() {
+    private void configSetup() {
+        if (!Files.exists(configDir)) {
+            try {
+                Files.createDirectories(configDir);
+            } catch (IOException io) {
+                io.printStackTrace();
+            }
+        }
+
         if (!Files.exists(configFile)) {
             try {
-                Files.createFile(configFile);
-                Sponge.getAssetManager().getAsset(this, "default.conf").get().copyToFile(configFile);
+                //noinspection OptionalGetWithoutIsPresent
+                Sponge.getAssetManager().getAsset(this, "config.conf").get().copyToFile(configFile);
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -98,7 +96,7 @@ public class AFMCorePlugin {
         }
     }
 
-    public static CommentedConfigurationNode getConfig() {
+    public static CommentedConfigurationNode getConfig(){
         return configNode;
     }
 }
