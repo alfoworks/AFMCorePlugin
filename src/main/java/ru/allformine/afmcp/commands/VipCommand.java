d2672a6e3130c729cf365f43a18b48c3862f99b7
HeroBrine1st <herobrine1.videos@gmail.com>
2019-04-24 17:09:35 +0300

    Ебанули /vip

--- src/main/java/ru/allformine/afmcp/commands/VipCommand.java
+++ src/main/java/ru/allformine/afmcp/commands/VipCommand.java
@@ -6,14 +6,40 @@ import org.spongepowered.api.command.CommandResult;
 import org.spongepowered.api.command.CommandSource;
 import org.spongepowered.api.command.args.CommandContext;
 import org.spongepowered.api.command.spec.CommandExecutor;
+import org.spongepowered.api.entity.living.player.Player;
+import org.spongepowered.api.text.Text;
 import ru.allformine.afmcp.AFMCorePlugin;
 
 import java.util.Map;
 
 public class VipCommand implements CommandExecutor {
     private ConfigurationNode configNode = AFMCorePlugin.getConfig();
-    private Map<Object, ? extends ConfigurationNode> vips = configNode.getChildrenMap();
+    private Map<Object, ? extends ConfigurationNode> vips = configNode.getNode("vips").getChildrenMap();
     public CommandResult execute(CommandSource source, CommandContext args) throws CommandException{
-        return CommandResult.success();
+        String vipToBuy = args.<String>getOne(Text.of("selectedVip")).get();
+        if(vipToBuy == null){
+            StringBuilder builder = new StringBuilder("Доступно к покупке:\n"); // TODO: дописать строку
+            for (Map.Entry<Object, ? extends ConfigurationNode> entry : vips.entrySet()) {
+                Object key = entry.getKey();
+                ConfigurationNode value = entry.getValue();
+                Integer cost = value.getNode("cost").getInt();
+                String name = key.toString();
+                builder.append(name).append(": ").append(cost).append(" токенов.\n");
+            }
+            source.sendMessage(Text.of(builder));
+            return CommandResult.success();
+        }
+        if(source instanceof Player) {
+            ConfigurationNode vipNode = configNode.getNode("vips", vipToBuy);
+            Integer cost = vipNode.getNode("cost").getInt();
+            String fullName = vipNode.getNode("fullName").getString();
+            // TODO: покупка привелегии
+            source.sendMessage(Text.of("Вы купили привелегию " + fullName + " за " + cost + " токенов."));
+            // Sponge.getServer(). броадкаст , если нужно
+            return CommandResult.success();
+        }else{
+            source.sendMessage(Text.of("Вы не являетесь игроком."));
+            throw new CommandException(Text.of("NonePlayerObject"));
+        }
     }
 }
