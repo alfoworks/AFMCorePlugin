ac9830ea5ea8a8e5af1aec3afe415466e8e3b2d3
Rob <rob.bog@list.ru>
2019-06-13 20:48:59 +0300

    СКОЛЬКО МОЖНО БЛЯДЬ ЕБАТЬСЯ С ЭТОЙ ПАРАШЕЙ???????????? АаааааааАААААААААААААААААААААААААА

--- src/main/java/ru/allformine/afmcp/commands/AFMCPCommand.java
+++ src/main/java/ru/allformine/afmcp/commands/AFMCPCommand.java
@@ -24,6 +24,6 @@ public class AFMCPCommand implements CommandExecutor {
     }
 
     void reply(CommandSource source, Text text) {
-        source.sendMessage(TextTemplate.of(getColor(), getName(), TextColors.WHITE, " > ", text).toText());
+        source.sendMessage(TextTemplate.of("", getColor(), getName(), TextColors.WHITE, " > ", text).toText());
     }
 }
