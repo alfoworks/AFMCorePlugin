c13852bec0318c1cfe4474d30505d5cea59a9b46
HeroBrine1st <herobrine1.videos@gmail.com>
2019-06-28 13:18:04 +0300

    Переработан /tokens под новый класс

--- src/main/java/ru/allformine/afmcp/commands/TokensCommand.java
+++ src/main/java/ru/allformine/afmcp/commands/TokensCommand.java
@@ -1,6 +1,5 @@
 package ru.allformine.afmcp.commands;
 
-import ninja.leaping.configurate.ConfigurationNode;
 import org.spongepowered.api.command.CommandResult;
 import org.spongepowered.api.command.CommandSource;
 import org.spongepowered.api.command.args.CommandContext;
@@ -9,22 +8,19 @@ import org.spongepowered.api.text.Text;
 import org.spongepowered.api.text.TextTemplate;
 import org.spongepowered.api.text.format.TextColor;
 import org.spongepowered.api.text.format.TextColors;
-import ru.allformine.afmcp.AFMCorePlugin;
-import ru.allformine.afmcp.net.http.GETResponse;
-import ru.allformine.afmcp.net.http.Requests;
+import ru.allformine.afmcp.net.api.Eco;
+
+import java.util.OptionalInt;
 
 public class TokensCommand extends AFMCPCommand {
-    private ConfigurationNode configNode = AFMCorePlugin.getConfig().getNode("eco");
-    private String key = configNode.getNode("key").getString();
-    private String balanceUrl = configNode.getNode("balanceApiUrl").getString();
 
     public CommandResult execute(CommandSource source, CommandContext args) {
         if (source instanceof Player) {
-            String url = balanceUrl + "&act=get&key=" + key + "&nick=" + source.getName();
-            GETResponse response = Requests.sendGet(url);
+            Eco eco = new Eco((Player) source);
+            OptionalInt balance = eco.getBalance();
 
-            if (response != null && response.responseCode == 200) {
-                reply(source, TextTemplate.of("Ваш баланс: ", getColor(), response.response, " токенов", TextColors.WHITE, ".").toText());
+            if (balance.isPresent()) {
+                reply(source, TextTemplate.of("Ваш баланс: ", getColor(), balance.getAsInt(), " токенов", TextColors.WHITE, ".").toText());
             } else {
                 reply(source, Text.of("Произошла неизвестная ошибка."));
             }
