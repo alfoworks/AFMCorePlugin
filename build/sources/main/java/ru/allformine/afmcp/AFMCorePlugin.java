9f51a0a6b10b4f62b383845f1eb5b6ad4129d0a0
HeroBrine1st <herobrine1.videos@gmail.com>
2019-07-02 12:26:35 +0300

    Ну иди нахуй, а

---
+++ build/sources/main/java/ru/allformine/afmcp/AFMCorePlugin.java
@@ -0,0 +1,219 @@
+package ru.allformine.afmcp;
+
+import com.google.inject.Inject;
+import io.netty.channel.ChannelFuture;
+import net.minecraft.network.NetworkSystem;
+import net.minecraft.server.MinecraftServer;
+import ninja.leaping.configurate.commented.CommentedConfigurationNode;
+import ninja.leaping.configurate.hocon.HoconConfigurationLoader;
+import ninja.leaping.configurate.loader.ConfigurationLoader;
+import org.apache.commons.lang3.ArrayUtils;
+import org.slf4j.Logger;
+import org.spongepowered.api.Platform;
+import org.spongepowered.api.Sponge;
+import org.spongepowered.api.command.args.GenericArguments;
+import org.spongepowered.api.command.spec.CommandSpec;
+import org.spongepowered.api.config.ConfigDir;
+import org.spongepowered.api.entity.living.player.Player;
+import org.spongepowered.api.event.Listener;
+import org.spongepowered.api.event.game.state.GameAboutToStartServerEvent;
+import org.spongepowered.api.event.game.state.GameInitializationEvent;
+import org.spongepowered.api.event.game.state.GamePreInitializationEvent;
+import org.spongepowered.api.event.game.state.GameStoppingServerEvent;
+import org.spongepowered.api.network.ChannelBinding;
+import org.spongepowered.api.network.PlayerConnection;
+import org.spongepowered.api.plugin.Plugin;
+import org.spongepowered.api.scheduler.Task;
+import org.spongepowered.api.text.Text;
+import ru.allformine.afmcp.commands.RawBCCommand;
+import ru.allformine.afmcp.commands.RestartCommand;
+import ru.allformine.afmcp.commands.TokensCommand;
+import ru.allformine.afmcp.commands.VipCommand;
+import ru.allformine.afmcp.handlers.DiscordWebhookListener;
+import ru.allformine.afmcp.handlers.VanishEventListener;
+import ru.allformine.afmcp.jumppad.JumpPadEventListener;
+import ru.allformine.afmcp.net.api.Webhook;
+import ru.allformine.afmcp.serverapi.HTTPServer;
+import ru.allformine.afmcp.test.PacketHandler;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
+
+import static java.lang.Math.min;
+
+@Plugin(
+        id = "afmcp",
+        name = "AFMCorePlugin",
+        description = "A plugin for a couple of random tasks.",
+        url = "http://allformine.ru",
+        authors = {
+                "Iterator, HeroBrine1st_Erq"
+        }
+)
+public class AFMCorePlugin {
+    public static boolean serverRestart = false;
+    @Inject
+    public static Logger logger;
+    private static CommentedConfigurationNode configNode;
+
+    private HTTPServer apiServer;
+    private Task apiServerTask;
+
+    public static Map<String, ChannelBinding.RawDataChannel> channel = new HashMap<>();
+
+    @Inject
+    @ConfigDir(sharedRoot = false)
+    private Path configDir;
+    private Path configFile;
+    private ConfigurationLoader<CommentedConfigurationNode> configLoader;
+
+    public static long startTime = 0;
+
+    public static CommentedConfigurationNode getConfig() {
+        return configNode;
+    }
+
+    @Inject
+    private void setLogger(Logger logger) {
+        AFMCorePlugin.logger = logger;
+    }
+
+    @Listener
+    public void preInit(GamePreInitializationEvent event) {
+        Sponge.getEventManager().registerListeners(this, new DiscordWebhookListener());
+        Sponge.getEventManager().registerListeners(this, new VanishEventListener());
+        Sponge.getEventManager().registerListeners(this, new JumpPadEventListener());
+
+        configFile = configDir.resolve("config.conf");
+        configLoader = HoconConfigurationLoader.builder().setPath(configFile).build();
+
+        configSetup();
+
+        CommandSpec restartCommandSpec = CommandSpec.builder()
+                .description(Text.of("Команда для перезагрузки сервера, единственная верная."))
+                .permission("afmcp.admin")
+                .executor(new RestartCommand())
+                .build();
+
+        Sponge.getCommandManager().register(this, restartCommandSpec, "afmrestart", "servrestart");
+
+        CommandSpec tokensCommandSpec = CommandSpec.builder()
+                .description(Text.of("Команда для получения количества токенов."))
+                .executor(new TokensCommand())
+                .build();
+
+        Sponge.getCommandManager().register(this, tokensCommandSpec, "tokens");
+
+        CommandSpec vipCommandSpec = CommandSpec.builder()
+                .description(Text.of("Команда для покупки привелегий."))
+                .executor(new VipCommand())
+                .arguments(
+                        GenericArguments.optional(GenericArguments.string(Text.of("selectedVip")))
+                )
+                .build();
+
+        Sponge.getCommandManager().register(this, vipCommandSpec, "vip");
+
+        CommandSpec rawBCspec = CommandSpec.builder()
+                .description(Text.of("Отправить текст в чат в таком виде, в каком вы его пишете."))
+                .executor(new RawBCCommand())
+                .arguments(
+                        GenericArguments.remainingJoinedStrings(Text.of("text"))
+                )
+                .build();
+
+        Sponge.getCommandManager().register(this, rawBCspec, "rawbc");
+    }
+
+    @Listener
+    public void init(GameInitializationEvent event) {
+        channel.put("screenshot", Sponge.getGame()
+                .getChannelRegistrar()
+                .createRawChannel(this, "AN3234234A"));
+
+        channel.get("screenshot").addListener(Platform.Type.SERVER, (buf, con, side) -> {
+            if (!(con instanceof PlayerConnection)) {
+                return;
+            }
+
+            Player player = ((PlayerConnection) con).getPlayer();
+
+            if (apiServer.playerScreenshotConfirmation.get(player) == null) {
+                return;
+            }
+
+            boolean isEnd = buf.readBoolean();
+
+            if (isEnd) {
+                apiServer.playerScreenshotConfirmation.replace(player, true);
+            } else {
+                byte[] chunkByteArray = buf.readBytes(min(10240, buf.available()));
+                byte[] prevArr = apiServer.playerScreenshotData.get(player);
+
+                apiServer.playerScreenshotData.replace(player, ArrayUtils.addAll(prevArr, chunkByteArray));
+            }
+        });
+
+        NetworkSystem networkSystem = ((MinecraftServer) Sponge.getServer()).getNetworkSystem();
+        for (ChannelFuture channelFuture : networkSystem.endpoints) {
+            channelFuture.channel().pipeline().addFirst(new PacketHandler());
+        }
+    }
+
+    @Listener
+    public void onServerStart(GameAboutToStartServerEvent event) {
+        startTime = System.currentTimeMillis();
+
+        //Discord.sendMessageServer(Discord.MessageTypeServer.TYPE_SERVER_STARTED);
+        Webhook.sendServerMessage(Webhook.TypeServerMessage.SERVER_STARTED);
+
+        this.apiServer = new HTTPServer();
+
+        apiServerTask = Task.builder().execute(this.apiServer)
+                .async().name("AFMCP APISERVER")
+                .submit(this);
+    }
+
+    @Listener
+    public void onServerStop(GameStoppingServerEvent event) {
+        //Discord.sendMessageServer(Discord.MessageTypeServer.TYPE_SERVER_STOPPED);
+        if (serverRestart) {
+            Webhook.sendServerMessage(Webhook.TypeServerMessage.SERVER_RESTARTING);
+        } else {
+            Webhook.sendServerMessage(Webhook.TypeServerMessage.SERVER_STOPPED);
+        }
+        apiServerTask.cancel();
+    }
+
+    private void configSetup() {
+        if (!Files.exists(configDir)) {
+            try {
+                Files.createDirectories(configDir);
+            } catch (IOException io) {
+                io.printStackTrace();
+            }
+        }
+
+        if (!Files.exists(configFile)) {
+            try {
+                //noinspection OptionalGetWithoutIsPresent
+                Sponge.getAssetManager().getAsset(this, "config.conf").get().copyToFile(configFile);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        load();
+    }
+
+    private void load() {
+        try {
+            configNode = configLoader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ build/sources/main/java/ru/allformine/afmcp/handlers/DiscordWebhookListener.java
@@ -0,0 +1,73 @@
+package ru.allformine.afmcp.handlers;
+
+import br.net.fabiozumbi12.UltimateChat.Sponge.API.SendChannelMessageEvent;
+import org.spongepowered.api.entity.living.player.Player;
+import org.spongepowered.api.event.Listener;
+import org.spongepowered.api.event.advancement.AdvancementEvent;
+import org.spongepowered.api.event.command.SendCommandEvent;
+import org.spongepowered.api.event.entity.DestructEntityEvent;
+import org.spongepowered.api.event.filter.cause.First;
+import org.spongepowered.api.event.network.ClientConnectionEvent;
+import ru.allformine.afmcp.net.api.Webhook;
+
+public class DiscordWebhookListener {
+    @Listener
+    public void onPlayerJoin(ClientConnectionEvent.Join event) {
+        Webhook.TypePlayerMessage type = event.getTargetEntity().hasPlayedBefore() ?
+                Webhook.TypePlayerMessage.JOINED_SERVER :
+                Webhook.TypePlayerMessage.JOINED_FIRST_TIME;
+        Webhook.sendPlayerMessage(type, event.getTargetEntity());
+    }
+
+    @Listener
+    public void onPlayerQuit(ClientConnectionEvent.Disconnect event) {
+        Webhook.sendPlayerMessage(Webhook.TypePlayerMessage.LEFT_SERVER, event.getTargetEntity());
+    }
+
+    @Listener
+    public void onSendChannelMessageEvent(SendChannelMessageEvent event) {
+        if (event.getSender() instanceof Player) {
+            String channelName = event.getChannel().getName();
+            Player sender = (Player) event.getSender();
+            String message = event.getMessage().toPlain();
+            if (channelName.equalsIgnoreCase("Local")) {
+                int x = (int) sender.getLocation().getX();
+                int y = (int) sender.getLocation().getY();
+                int z = (int) sender.getLocation().getZ();
+                Webhook.sendPlayerMessage(Webhook.TypePlayerMessage.LVL2_CHAT_MESSAGE,
+                        sender,
+                        String.format("X: %s, Y: %s, Z: %s", x, y, z),
+                        channelName,
+                        message
+                );
+            } else {
+                Webhook.sendPlayerMessage(Webhook.TypePlayerMessage.CHAT_MESSAGE, sender, channelName, message);
+            }
+        }
+    }
+
+    @Listener
+    public void onCommandSend(SendCommandEvent event, @First Player player) {
+        Webhook.sendPlayerMessage(Webhook.TypePlayerMessage.COMMAND, player, event.getCommand() + " " + event.getArguments());
+    }
+
+    @Listener
+    public void onAdvancement(AdvancementEvent.Grant event) {
+        if (!event.getAdvancement().getName().startsWith("recipes_")) {
+            Webhook.sendPlayerMessage(Webhook.TypePlayerMessage.EARNED_ADVANCEMENT,
+                    event.getTargetEntity(),
+                    event.getAdvancement().getName()
+            );
+        }
+    }
+
+    @Listener
+    public void onDeath(DestructEntityEvent.Death event) {
+        if (event.getTargetEntity() instanceof Player) {
+            Webhook.sendPlayerMessage(Webhook.TypePlayerMessage.DIED,
+                    (Player) event.getTargetEntity(),
+                    event.getMessage().toPlain()
+            );
+        }
+    }
+}
