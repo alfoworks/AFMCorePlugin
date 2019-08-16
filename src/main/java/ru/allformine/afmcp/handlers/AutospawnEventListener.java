65ea7771b22dea7df86e32bbbcb2f02caa725702
HeroBrine1st_Erquilenne <123@mail.ru>
2019-08-16 16:21:45 +0300

    Автоспаун, ибо нуклеус ебланы делали

---
+++ src/main/java/ru/allformine/afmcp/handlers/AutospawnEventListener.java
@@ -0,0 +1,15 @@
+package ru.allformine.afmcp.handlers;
+
+import org.spongepowered.api.Sponge;
+import org.spongepowered.api.event.Listener;
+import org.spongepowered.api.event.network.ClientConnectionEvent;
+
+public class AutospawnEventListener {
+    @Listener
+    public void onPlayerJoin(ClientConnectionEvent.Join event) {
+        if (event.getTargetEntity().hasPlayedBefore()) {
+            Sponge.getCommandManager().process(Sponge.getServer().getConsole(),
+                    "spawn other " + event.getTargetEntity().getName());
+        }
+    }
+}
