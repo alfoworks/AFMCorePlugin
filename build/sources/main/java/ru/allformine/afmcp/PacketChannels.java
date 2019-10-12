3e8b8bc8b4a0281d56d823892104b99b717a099c
Rob <rob.bog@list.ru>
2019-10-13 01:06:25 +0300

    .gitignore is now working

--- build/sources/main/java/ru/allformine/afmcp/PacketChannels.java
+++
@@ -1,8 +0,0 @@
-package ru.allformine.afmcp;
-
-import org.spongepowered.api.network.ChannelBinding;
-
-public class PacketChannels {
-    public static ChannelBinding.RawDataChannel SCREENSHOT;
-    public static ChannelBinding.RawDataChannel FACTIONS;
-}
