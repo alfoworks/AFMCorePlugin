2bfe019ff97f089f5efc718ff2a747534f67e3f0
Rob <rob.bog@list.ru>
2020-01-05 11:47:22 +0300

    Исправления

--- src/main/java/ru/alfomine/afmcp/DiscordNotifyTask.java
+++ src/main/java/ru/alfomine/afmcp/DiscordNotifyTask.java
@@ -2,22 +2,20 @@ package ru.alfomine.afmcp;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Player;
 
 public class DiscordNotifyTask implements Runnable {
     @Override
     public void run() {
-        ConfigurationSection section = AFMCorePlugin.config.getConfigurationSection("playerDiscordConfirmations");
-
         for (Player player : Bukkit.getOnlinePlayers()) {
-            if (section.getKeys(false).contains(player.getName())) {
+            if (AFMCorePlugin.config.getString("playerDiscordConfirmations") != null) {
                 return;
             }
 
-            player.sendMessage(String.format("%sALFO:MINE %s> Вам нужно указать свой аккаунт в Discord (т.к. большинство его почему-то не читает", ChatColor.YELLOW, ChatColor.AQUA));
+            player.sendMessage(String.format("%sALFO:MINE %s> Вам нужно указать свой аккаунт в Discord (т.к. его никто не читает и никто не отписывается)", ChatColor.YELLOW, ChatColor.AQUA));
             player.sendMessage(String.format("%sЧтобы указать свой аккаунт, напишите /ds <nick>", ChatColor.GREEN));
             player.sendMessage(String.format("%sЕсли ваш ник уже записан, или вы не состоите в персонале сервера - напишите /ds none", ChatColor.RED));
+            player.sendMessage(String.format("%sЕсли вас вообще нет в Discord, то с подключением: https://discord.gg/Ucrrw64", ChatColor.LIGHT_PURPLE));
         }
     }
 }
