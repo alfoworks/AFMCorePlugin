022ead23fc3bc88244a374a365145502e74c4f01
Rob <rob.bog@list.ru>
2020-01-07 02:21:18 +0300

    критикал ошибка я хуёвый кодер аааааааааааааа

--- src/main/java/ru/alfomine/afmcp/packetlisteners/TranslatePacketListener.java
+++ src/main/java/ru/alfomine/afmcp/packetlisteners/TranslatePacketListener.java
@@ -4,6 +4,7 @@ import com.comphenix.protocol.PacketType;
 import com.comphenix.protocol.events.PacketAdapter;
 import com.comphenix.protocol.events.PacketEvent;
 import org.bukkit.plugin.Plugin;
+import ru.alfomine.afmcp.PluginStatics;
 import ru.alfomine.afmcp.net.YandexTranslator;
 
 public class TranslatePacketListener extends PacketAdapter {
@@ -13,6 +14,8 @@ public class TranslatePacketListener extends PacketAdapter {
 
     @Override
     public void onPacketSending(PacketEvent event) {
+        if (!PluginStatics.debugRetranslateEnabled) return;
+
         if (event.getPacketType() == PacketType.Play.Server.CHAT) {
             String retranslated = YandexTranslator.retranslate(event.getPacket().getStrings().read(0));
 
