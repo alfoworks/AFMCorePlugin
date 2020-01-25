cef009e088b8985c898a31f7a19b38ef989aa88a
HeroBrine1st Erquilenne <pankov.76.tvink@gmail.com>
2020-01-25 15:18:47 +0300

    Вот ваш фикс блять, сосите хуй

--- src/main/java/ru/alfomine/afmcp/tablist/WrappedTabList.java
+++ src/main/java/ru/alfomine/afmcp/tablist/WrappedTabList.java
@@ -127,7 +127,6 @@ public class WrappedTabList {
         this.entries.sort((a, b) -> {
             String aName = Iterables.getLast(a.permissionUser.getParentIdentifiers(null), "player");
             String bName = Iterables.getLast(b.permissionUser.getParentIdentifiers(null), "player");
-            AFMCorePlugin.log("a " + aName + "; b " + bName, Level.INFO);
             return Integer.compare(PluginConfig.tabSortGroups.indexOf(aName), PluginConfig.tabSortGroups.indexOf(bName));
         });
     }
