80b97efd0d0e5121f132e35643692f81c2f14663
HeroBrine1st Erquilenne <pankov.76.tvink@gmail.com>
2020-03-09 22:56:43 +0300

    Удаляем target/четотам

--- src/main/java/ru/allformine/afmcp/tablist/Tablist.kt
+++
@@ -1,2 +0,0 @@
-package ru.allformine.afmcp.tablist
-
---
+++ src/main/java/ru/allformine/afmcp/tablist/WrappedTabList.kt
@@ -0,0 +1,8 @@
+package ru.allformine.afmcp.tablist
+
+import org.spongepowered.api.entity.living.player.tab.TabListEntry
+
+class WrappedTabList {
+    val entries = ArrayList<TabListEntry>()
+
+}
