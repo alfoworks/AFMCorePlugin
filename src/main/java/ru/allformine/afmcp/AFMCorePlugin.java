e4748bb6c3f199d3316b6013956ce5b7ad1ce0b3
Rob <rob.bog@list.ru>
2020-03-09 23:57:42 +0300

    херня из под коня

--- src/main/java/ru/allformine/afmcp/AFMCorePlugin.java
+++ src/main/java/ru/allformine/afmcp/AFMCorePlugin.java
@@ -1,7 +1,6 @@
 package ru.allformine.afmcp;
 
 import com.google.inject.Inject;
-import net.luckperms.api.LuckPerms;
 import ninja.leaping.configurate.commented.CommentedConfigurationNode;
 import ninja.leaping.configurate.hocon.HoconConfigurationLoader;
 import ninja.leaping.configurate.loader.ConfigurationLoader;
@@ -17,7 +16,6 @@ import org.spongepowered.api.event.game.state.GameStartedServerEvent;
 import org.spongepowered.api.event.game.state.GameStoppingServerEvent;
 import org.spongepowered.api.plugin.Plugin;
 import org.spongepowered.api.scheduler.Task;
-import org.spongepowered.api.service.ProviderRegistration;
 import org.spongepowered.api.text.Text;
 import ru.allformine.afmcp.commands.*;
 import ru.allformine.afmcp.jumppad.JumpPadEventListener;
@@ -33,7 +31,8 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Objects;
-import java.util.Optional;
+
+//import net.luckperms.api.LuckPerms;
 
 @Plugin(
         id = "afmcp",
@@ -61,7 +60,7 @@ public class AFMCorePlugin {
     private Path configDir;
     private Path configFile;
     private static ConfigurationLoader<CommentedConfigurationNode> configLoader;
-    public static LuckPerms luckPerms;
+    //public static LuckPerms luckPerms;
 
     public static CommentedConfigurationNode getConfig() {
         return configNode;
@@ -85,8 +84,8 @@ public class AFMCorePlugin {
         PacketChannels.FACTIONS = Sponge.getGame()
                 .getChannelRegistrar()
                 .createRawChannel(this, "factions");
-        Optional<ProviderRegistration<LuckPerms>> provider = Sponge.getServiceManager().getRegistration(LuckPerms.class);
-        provider.ifPresent(luckPermsProviderRegistration -> this.luckPerms = luckPermsProviderRegistration.getProvider());
+        //Optional<ProviderRegistration<LuckPerms>> provider = Sponge.getServiceManager().getRegistration(LuckPerms.class);
+        //provider.ifPresent(luckPermsProviderRegistration -> this.luckPerms = luckPermsProviderRegistration.getProvider());
     }
 
     @Listener
