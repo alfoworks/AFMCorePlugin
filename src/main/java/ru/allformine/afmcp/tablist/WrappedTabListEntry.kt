22915f025f2c446bd4225f1b2675d7839d43f3e2
HeroBrine1st Erquilenne <pankov.76.tvink@gmail.com>
2020-03-15 11:23:55 +0300

    Так, пробуем запустить вот так

--- src/main/java/ru/allformine/afmcp/tablist/WrappedTabListEntry.kt
+++ src/main/java/ru/allformine/afmcp/tablist/WrappedTabListEntry.kt
@@ -4,12 +4,13 @@ import org.spongepowered.api.data.value.mutable.Value
 import org.spongepowered.api.entity.living.player.Player
 import org.spongepowered.api.entity.living.player.gamemode.GameMode
 import org.spongepowered.api.text.Text
+import ru.allformine.afmcp.AFMCorePlugin
 import java.util.*
 
 
 class WrappedTabListEntry(player: Player) {
-    //private val lpUser = AFMCorePlugin.luckPerms.userManager.getUser(player.uniqueId)
-    //val priority = lpUser?.primaryGroup?.get(0)?.toByte()?.toInt() ?: 0
+    private val lpUser = AFMCorePlugin.luckPerms.userManager.getUser(player.uniqueId)
+    val priority = lpUser?.primaryGroup?.get(0)?.toByte()?.toInt() ?: 0
     val name = Text.of("1234")
     var latency = player.connection.latency
     val uuid: UUID = player.uniqueId
