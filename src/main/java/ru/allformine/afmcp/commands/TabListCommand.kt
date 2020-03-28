0c2afe5c9110a9c4e63cac94b39143000425a54a
HeroBrine1st Erquilenne <pankov.76.tvink@gmail.com>
2020-03-28 22:36:40 +0300

    All For Debug v2

--- src/main/java/ru/allformine/afmcp/commands/TabListCommand.kt
+++ src/main/java/ru/allformine/afmcp/commands/TabListCommand.kt
@@ -9,7 +9,7 @@ import ru.allformine.afmcp.tablist.WrappedTabList
 class TabListCommand: AFMCPCommand() {
     override fun execute(src: CommandSource?, args: CommandContext?): CommandResult? {
         var replyStr = "Игроки: "
-        WrappedTabList.entries.forEach { replyStr += "%s ".format(it.name)}
+        WrappedTabList.entries.forEach { replyStr += "%s ".format(it.toString())}
         replyString(src, replyStr)
         return CommandResult.success()
     }
