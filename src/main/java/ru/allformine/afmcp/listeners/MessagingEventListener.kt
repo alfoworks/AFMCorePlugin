ae7bf3c48a970086db197e1f294d7526c66b08c8
Rob <rob.bog@list.ru>
2020-04-12 01:27:15 +0300

    FirstSexEventListener.java -> MessagingEventListener.kt

--- src/main/java/ru/allformine/afmcp/listeners/MessagingEventListener.kt
+++ src/main/java/ru/allformine/afmcp/listeners/MessagingEventListener.kt
@@ -1,14 +1,15 @@
-package ru.allformine.afmcp.listeners;
+package ru.allformine.afmcp.listeners
 
-import org.spongepowered.api.event.Listener;
-import org.spongepowered.api.event.network.ClientConnectionEvent;
-import ru.allformine.afmcp.Messaging;
+import org.spongepowered.api.event.Listener
+import org.spongepowered.api.event.network.ClientConnectionEvent.Join
+import ru.allformine.afmcp.Messaging
+import ru.allformine.afmcp.Messaging.sendMessage
 
-public class FirstSexEventListener {
+class MessagingEventListener {
     @Listener
-    public void onPlayerJoin(ClientConnectionEvent.Join event) {
-        if (!event.getTargetEntity().hasPlayedBefore()) {
-            Messaging.sendMessage(event.getTargetEntity(), "Привет! Добро пожаловать на ALFO:MINE. Мы рады тебя видеть.\nсосни хуй", Messaging.MessageType.WINDOWED);
+    fun onPlayerJoin(event: Join) {
+        if (!event.targetEntity.hasPlayedBefore()) {
+            sendMessage(event.targetEntity, "Привет! Добро пожаловать на ALFO:MINE. Мы рады тебя видеть.\nсосни хуй", Messaging.MessageType.WINDOWED)
         }
     }
 }
