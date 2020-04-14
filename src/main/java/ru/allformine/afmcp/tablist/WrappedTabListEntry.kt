d7f9845bd04db8cdb2e53b896514d2a387eb79ea
HeroBrine1st Erquilenne <pankov.76.tvink@gmail.com>
2020-04-14 19:26:25 +0300

    [FIX] Таблист. 1. Лишняя строка в заголовке и подвале убрана. 2. Количество игроков теперь показывается с учетом ваниша. 3. Убран лишний пробел после префикса.

--- src/main/java/ru/allformine/afmcp/tablist/WrappedTabListEntry.kt
+++ src/main/java/ru/allformine/afmcp/tablist/WrappedTabListEntry.kt
@@ -1,6 +1,7 @@
 package ru.allformine.afmcp.tablist
 
 import org.spongepowered.api.Sponge
+import org.spongepowered.api.data.key.Keys
 import org.spongepowered.api.data.value.mutable.Value
 import org.spongepowered.api.entity.living.player.Player
 import org.spongepowered.api.entity.living.player.gamemode.GameMode
@@ -23,7 +24,7 @@ class WrappedTabListEntry(val player: Player) {
             .find { player.hasPermission("group.%s".format(it.string)) }
             ?.let { PluginConfig.tablistSorting.childrenList.indexOf(it) } ?: 2147483647
     private val prefix = player.getOption("prefix").orElse("")!!
-    val name: LiteralText = Text.of(prefix + " " + player.name)
+    val name: LiteralText = Text.of(prefix + player.name)
     val latency = player.connection.latency
     val uuid: UUID = player.uniqueId
     val gameMode: Value<GameMode> = player.gameMode()
@@ -40,16 +41,15 @@ class WrappedTabListEntry(val player: Player) {
     private fun generateDynamicStuff(player: Player) {
         val playerLocation = player.location
 
-        header = TextSerializers.FORMATTING_CODE.deserialize(PluginConfig.tabListHeader + "\n" +
-                    String.format(PluginConfig.tabListOnlineCount,
-                            Sponge.getServer().onlinePlayers.size, Sponge.getServer().maxPlayers))
-        footer = TextSerializers.FORMATTING_CODE.deserialize(PluginConfig.tabListFooter + "\n" +
+        header = TextSerializers.FORMATTING_CODE.deserialize(PluginConfig.tabListHeader +
+                if(PluginConfig.tabListOnlineCount.isBlank()) "" else
+                    "\n" + String.format(PluginConfig.tabListOnlineCount,
+                        Sponge.getServer().onlinePlayers.filter { !it.keys.contains(Keys.VANISH) }.size,
+                        Sponge.getServer().maxPlayers))
+        footer = TextSerializers.FORMATTING_CODE.deserialize(PluginConfig.tabListFooter +
+                if(PluginConfig.tabListCoordinates.isBlank()) "" else
+                "\n" +
                 String.format(PluginConfig.tabListCoordinates, playerLocation.blockX, playerLocation.blockY, playerLocation.blockZ))
-    }
 
-//    fun WrappedTabListEntry(player: Player) {
-//        header = ChatColor.translateAlternateColorCodes('&', PluginConfig.tabListHeader)
-//        footer = ChatColor.translateAlternateColorCodes('&', PluginConfig.tabListFooter)
-//        generateDynamicStuff(player)
-//    }
+    }
 }
